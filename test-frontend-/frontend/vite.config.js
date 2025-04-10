074d340e3d7412dccbdd1dd812cefcd4bdb4b5b0
OJASWI-17 <ojaswijaswal2004@gmail.com>
2025-04-10 16:18:25 +0530

    balance api , delayed to 40

--- test-frontend-/frontend/vite.config.js
+++ test-frontend-/frontend/vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // 👈 Add this line
+    port: 3000,      // 👈 Optional: explicitly set port
+  },
 })
